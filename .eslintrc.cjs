4d908f00b41ffd07debc9d35ad8208e00f3ba639
ReinerLau <lk850593913@gmail.com>
2024-08-09 13:29:28 +0800

    build: eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
   ],
   plugins: ["@typescript-eslint", "vue"],
-};
+}
